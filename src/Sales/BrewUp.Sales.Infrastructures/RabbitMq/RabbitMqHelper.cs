78bca53f680ef503749b1281048c3c5a331a6be8
Alberto Acerbis <alberto.acerbis@gmail.com>
2025-05-28 15:10:12 +0200

    Upgrade Muflone

--- src/Sales/BrewUp.Sales.Infrastructures/RabbitMq/RabbitMqHelper.cs
+++ src/Sales/BrewUp.Sales.Infrastructures/RabbitMq/RabbitMqHelper.cs
@@ -1,12 +1,10 @@
-﻿using BrewUp.Sales.Infrastructures.RabbitMq.Commands;
-using BrewUp.Sales.Infrastructures.RabbitMq.Events;
-using BrewUp.Sales.ReadModel.Services;
+﻿using BrewUp.Sales.Acl;
+using BrewUp.Sales.Domain.CommandHandlers;
+using BrewUp.Sales.ReadModel.EventHandlers;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Muflone;
-using Muflone.Persistence;
 using Muflone.Transport.RabbitMQ;
-using Muflone.Transport.RabbitMQ.Factories;
 using Muflone.Transport.RabbitMQ.Models;
 
 namespace BrewUp.Sales.Infrastructures.RabbitMq;
@@ -17,38 +15,19 @@ public static class RabbitMqHelper
     RabbitMqSettings rabbitMqSettings)
   {
     var serviceProvider = services.BuildServiceProvider();
-    var repository = serviceProvider.GetRequiredService<IRepository>();
     var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
 
     var rabbitMqConfiguration = new RabbitMQConfiguration(rabbitMqSettings.Host, rabbitMqSettings.Username,
       rabbitMqSettings.Password, rabbitMqSettings.ExchangeCommandName, rabbitMqSettings.ExchangeEventName, rabbitMqSettings.ClientId);
-    var mufloneConnectionFactory = new RabbitMQConnectionFactory(rabbitMqConfiguration, loggerFactory);
 
     services.AddMufloneTransportRabbitMQ(loggerFactory, rabbitMqConfiguration);
 
-    serviceProvider = services.BuildServiceProvider();
-    var consumers = serviceProvider.GetRequiredService<IEnumerable<IConsumer>>();
-    consumers = consumers.Concat(new List<IConsumer>
-    {
-      new CreateSalesOrderConsumer(repository,
-        mufloneConnectionFactory,
-        loggerFactory),
-      new SalesOrderCreatedConsumer(serviceProvider.GetRequiredService<ISalesOrderService>(),
-        serviceProvider.GetRequiredService<IEventBus>(),
-        mufloneConnectionFactory, loggerFactory),
+    services.AddCommandHandler<CreateSalesOrderCommandHandler>();
+    services.AddCommandHandler<UpdateAvailabilityDueToWarehousesNotificationCommandHandler>();
 
-      new AvailabilityUpdatedForNotificationConsumer(serviceProvider.GetRequiredService<IServiceBus>(),
-        mufloneConnectionFactory,
-        loggerFactory),
-
-      new UpdateAvailabilityDueToWarehousesNotificationConsumer(repository, mufloneConnectionFactory,
-        loggerFactory),
-      new AvailabilityUpdatedDueToWarehousesNotificationConsumer(serviceProvider.GetRequiredService<IAvailabilityService>(),
-                mufloneConnectionFactory,
-                        loggerFactory)
-    });
-
-    services.AddMufloneRabbitMQConsumers(consumers);
+    services.AddDomainEventHandler<AvailabilityUpdatedDueToWarehousesNotificationEventHandler>();
+    services.AddDomainEventHandler<SalesOrderCreatedEventHandlerAsync>();
+    services.AddIntegrationEventHandler<AvailabilityUpdatedForNotificationEventHandler>();
 
     return services;
   }
