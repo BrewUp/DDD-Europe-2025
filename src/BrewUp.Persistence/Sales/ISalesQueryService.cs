4d96990a5aa8f5a13ca86d6eec14c9480c7883be
Arialdo Martini <arialdo.martini@gmail.com>
2025-11-27 10:26:56 +0100

    ISalesQueryService was unused

--- src/BrewUp.Persistence/Sales/ISalesQueryService.cs
+++
@@ -1,9 +0,0 @@
-﻿using BrewUp.Shared.Contracts;
-using BrewUp.Shared.Entities;
-
-namespace BrewUp.Persistence.Sales;
-
-public interface ISalesQueryService
-{
-    Task<PagedResult<SalesOrderJson>> GetSalesOrdersAsync(int page, int pageSize);
-}
